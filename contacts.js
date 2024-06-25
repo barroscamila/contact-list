094c667fa2df9360a7236997a67cdad149688da5
barroscamila <camilagdbarros@gmail.com>
2024-06-25 20:01:57 -0300

    ajuste contacts page

--- contacts.js
+++ contacts.js
@@ -7,16 +7,27 @@ async function showContacts() {
     const contacts = await response.json()
     // console.log(contacts)
     contacts.forEach(contact => {
-        console.log(contact)
+        // console.log(contact)
         createContactCard(contact.fullname, contact.nickname, contact.birthday, contact.number, contact.number2, contact.email, contact.postcode, contact.address, contact.numberAddress, contact.city, contact.state, contact.country, contact.instagram, contact.xtwitter, contact.linkedin)
     })
 }
 
+// funcao para verificar se o campo é null
+// function isInfoNull(contactInfos) {
+//     contactInfos.forEach(info => { return (info = "" ? "-" : info) });
+// }
+
+// funcao para criar card dos contatos cadastrados
 function createContactCard(fullname, nickname, birthday, number, number2, email, postcode, address, numberAddress, city, state, country, instagram, xtwitter, linkedin) {
     const card = document.createElement('div')
     card.classList = 'contactCard'
+
+    // const contactInfos = [fullname, nickname, birthday, number, number2, email, postcode, address, numberAddress, city, state, country, instagram, xtwitter, linkedin]
+    // const teste = isInfoNull(contactInfos)
+    // console.log(teste)
+
     card.innerHTML = `
-    <p><span><strong>${fullname}</strong></span> <span>(${nickname})</span></p>
+    <p><span id="contactName"><strong>${fullname}</strong></span> <span>(${nickname})</span></p>
     <p><strong>Birthday:</strong> <span>${birthday}</span></p>
     <p><strong>Contact numbers:</strong> <span>${number}</span>, <span>${number2}</span></p>
     <p>E-mail: <span>${email}</span></p>
